aeb2d16886179dd0bca39771786c536120195793
PPTenshiKanata <russellyaplong@gmail.com>
2022-07-22 12:33:20 +0800

    DarrenPush

---
+++ cufhe/prove/sockets/Sample/fileClient.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#define SIZE 1024
+
+void send_file(FILE *fp, int sockfd){
+  int n;
+  char data[SIZE] = {0};
+
+  while(fgets(data, SIZE, fp) != NULL) {
+    if (send(sockfd, data, sizeof(data), 0) == -1) {
+      perror("[-]Error in sending file.");
+      exit(1);
+    }
+    bzero(data, SIZE);
+  }
+}
+
+void write_file(int sockfd){
+  int n;
+  FILE *fp;
+  char *filename = "results.txt";
+  char buffer[SIZE];
+
+  fp = fopen(filename, "w");
+  while (1) {
+    n = recv(sockfd, buffer, SIZE, 0);
+    if (n <= 0){
+      break;
+      return;
+    }
+    fprintf(fp, "%s", buffer);
+    bzero(buffer, SIZE);
+  }
+  return;
+}
+
+int main(){
+  // Change this IP	
+  char *ip = "69.69.69.1";
+  // Change this host port
+<<<<<<< HEAD
+  int port = 4381;
+=======
+  int port = 4380;
+>>>>>>> bc36fd060f54999ccb8fe41a0bed7a8b29624f46
+  int e;
+
+  int sockfd, new_sock;
+  struct sockaddr_in server_addr, new_addr;
+  FILE *fp;
+  // Change this file name and file path if you need
+  char *filename = "send.txt";
+
+  socklen_t addr_size;
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0) {
+    perror("[-]Error in socket");
+    exit(1);
+  }
+  printf("[+]Server socket created successfully.\n");
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = port;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+
+  e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if(e == -1) {
+    perror("[-]Error in socket");
+    exit(1);
+  }
+	printf("[+]Connected to Server.\n");
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    perror("[-]Error in reading file.");
+    exit(1);
+  }
+
+  send_file(fp, sockfd);
+  printf("[+]File data sent successfully.\n");
+  
+  printf("[+]Receiving file from server.. \n");
+
+  addr_size = sizeof(new_addr);
+  new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
+  write_file(new_sock);
+  printf("[+]Data written in the file successfully");
+
+  printf("[+]Closing the connection.\n");
+  close(sockfd);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
