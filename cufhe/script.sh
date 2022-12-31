b2bb26a8c0750d8e4f6cb962bf9640cc7fd1fa2f
darrenori <87930795+darrenori@users.noreply.github.com>
2022-12-31 12:20:12 +0800

    Add files via upload

--- cufhe/script.sh
+++ cufhe/script.sh
@@ -1,46 +1,23 @@
 #!/bin/bash
+clear
+#read -p "Enter a directory: " dire
+dire="prove";
 cd /root/cuFHE/cufhe/
 make
 
-dire="prove"
-nvcc -c $dire/newclient.cu
-nvcc -std=c++11 -O3 -w  -I./ -M -o $dire/test_api_gpu.d $dire/newclient.cu
-nvcc -std=c++11 -O3 -w  -I./ -c -o $dire/newclient.o $dire/newclient.cu
-nvcc -std=c++11 -O3 -w  -o $dire/newclient $dire/newclient.o -Lbin -lcufhe_gpu
-exit
+#nvcc -c $dire/test_api_gpu.cu
+#nvcc -std=c++11 -O3 -w  -I./ -M -o $dire/test_api_gpu.d $dire/test_api_gpu.cu
+#nvcc -std=c++11 -O3 -w  -I./ -c -o $dire/test_api_gpu.o $dire/test_api_gpu.cu
+#nvcc -std=c++11 -O3 -w  -o $dire/test_api_gpu $dire/test_api_gpu.o -Lbin -lcufhe_gpu
 
-dire="Client1"
-nvcc -c $dire/client1.cu
-nvcc -std=c++11 -O3 -w  -I./ -M -o $dire/test_api_gpu.d $dire/client1.cu
-nvcc -std=c++11 -O3 -w  -I./ -c -o $dire/client1.o $dire/client1.cu
-nvcc -std=c++11 -O3 -w  -o $dire/client1 $dire/client1.o -Lbin -lcufhe_gpu
+#nvcc -c $dire/testing.cu
+#nvcc -std=c++11 -O3 -w  -I./ -M -o $dire/test_api_gpu.d $dire/testing.cu
+#nvcc -std=c++11 -O3 -w  -I./ -c -o $dire/testing.o $dire/testing.cu
+#nvcc -std=c++11 -O3 -w  -o $dire/testing $dire/testing.o -Lbin -lcufhe_gpu
 
-dire="Client2"
-nvcc -c $dire/client2.cu
-nvcc -std=c++11 -O3 -w  -I./ -M -o $dire/test_api_gpu.d $dire/client2.cu
-nvcc -std=c++11 -O3 -w  -I./ -c -o $dire/client2.o $dire/client2.cu
-nvcc -std=c++11 -O3 -w  -o $dire/client2 $dire/client2.o -Lbin -lcufhe_gpu
-
-dire="Client2"
-nvcc -c $dire/client2.cu
-nvcc -std=c++11 -O3 -w  -I./ -M -o $dire/test_api_gpu.d $dire/client2.cu
-nvcc -std=c++11 -O3 -w  -I./ -c -o $dire/client2.o $dire/client2.cu
-nvcc -std=c++11 -O3 -w  -o $dire/client2 $dire/client2.o -Lbin -lcufhe_gpu
-
-dire="Keygen"
-nvcc -c $dire/keygen.cu
-nvcc -std=c++11 -O3 -w  -I./ -M -o $dire/test_api_gpu.d $dire/keygen.cu
-nvcc -std=c++11 -O3 -w  -I./ -c -o $dire/keygen.o $dire/keygen.cu
-nvcc -std=c++11 -O3 -w  -o $dire/keygen $dire/keygen.o -Lbin -lcufhe_gpu
-
-dire="Verif"
-nvcc -c $dire/verif.cu
-nvcc -std=c++11 -O3 -w  -I./ -M -o $dire/test_api_gpu.d $dire/verif.cu
-nvcc -std=c++11 -O3 -w  -I./ -c -o $dire/verif.o $dire/verif.cu
-nvcc -std=c++11 -O3 -w  -o $dire/verif $dire/verif.o -Lbin -lcufhe_gpu
-
-dire="Server"
 nvcc -c $dire/newserver.cu
 nvcc -std=c++11 -O3 -w  -I./ -M -o $dire/test_api_gpu.d $dire/newserver.cu
 nvcc -std=c++11 -O3 -w  -I./ -c -o $dire/newserver.o $dire/newserver.cu
 nvcc -std=c++11 -O3 -w  -o $dire/newserver $dire/newserver.o -Lbin -lcufhe_gpu
+#clear
+#./prove/test_api_gpu
